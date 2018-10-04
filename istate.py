1a503e33b4d6d77504e2df2000dc6043a3a13d2f
Amr <amressam01@hotmail.com>
2018-10-04 21:35:05 +0200

    Apply PEP8

--- istate.py
+++ istate.py
@@ -4,19 +4,19 @@ from time import sleep
 from sys import argv, platform
 
 try:
-	while True:
-		x = system('ping google.com -c 4 -t 3')
-		
-		if x == 0:
-			print("up")
-			#if platform == 'darwin': system("say internet is up")
-		else:
-			print("down")
-			#if platform == 'darwin': system("say internet is down")
-		
-		sleep(int(argv[1]) if len(argv) > 1 else 27)
+    while True:
+        x = system('ping google.com -c 4 -t 3')
+
+        if x == 0:
+            print("up")
+            #if platform == 'darwin': system("say internet is up")
+        else:
+            print("down")
+            #if platform == 'darwin': system("say internet is down")
+
+        sleep(int(argv[1]) if len(argv) > 1 else 27)
 
 except KeyboardInterrupt:
-	print()
-	exit(0)
-#sat dec 16 4:11 pm
+    print()
+    exit(0)
+# sat dec 16 4:11 pm
